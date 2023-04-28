123a32547eb173198cc8f74cc0d505b1fa486491
Saketku98 <88224770+Saketku98@users.noreply.github.com>
2023-04-28 23:37:54 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Todos from './Component/todos';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='App-header'>
+      <Todos/>
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
